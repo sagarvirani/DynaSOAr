e2e197d224a8c2b622a29c2f0de98d7ccb7a3c9f
Sagar Virani <sagarvirani@gmail.com>
2012-01-24 16:04:13 +0530

    Implemented file copy from WARdir directory to deploy directory if the value of deployed filed in the json file is true

--- src/org/dynasoar/service/DynasoarService.java
+++ src/org/dynasoar/service/DynasoarService.java
@@ -3,8 +3,9 @@ package org.dynasoar.service;
 public class DynasoarService {
 	private String shortName = null;
 	private String name = null;
+        private String value = null;
 
-	public String getShortName() {
+        public String getShortName() {
 		return shortName;
 	}
 
@@ -19,4 +20,10 @@ public class DynasoarService {
 	public void setName(String name) {
 		this.name = name;
 	}
+        public void setdeployed(String value) {
+                this.value = value;
+        }
+        public String getdeployed() {
+                return value;
+        }
 }
--- src/org/dynasoar/service/ServiceMonitor.java
+++ src/org/dynasoar/service/ServiceMonitor.java
@@ -1,91 +1,163 @@
 package org.dynasoar.service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import org.apache.log4j.Logger;
 import org.dynasoar.sync.ChangeEvent;
 import org.dynasoar.sync.DirectoryWatcher;
 import org.dynasoar.config.Configuration;
 import java.util.*;
+import java.util.logging.Level;
 
 /**
  * ServiceMonitor is responsible for monitoring changes in Service config files.
  * It is supposed to act on and notify NodeCommunicator of any change in
  * Service.
- * 
+ *
  * @author Rakshit Menpara
  */
 public class ServiceMonitor implements Runnable {
 
-	private static ServiceMonitor current = null;
-	private static Logger logger = Logger.getLogger(ServiceMonitor.class);
-	private static Thread th = null;
-	private static HashMap<String, DynasoarService> serviceMap = new HashMap<String, DynasoarService>();
+    private static ServiceMonitor current = null;
+    private static Logger logger = Logger.getLogger(ServiceMonitor.class);
+    private static Thread th = null;
+    private static HashMap<String, DynasoarService> serviceMap = new HashMap<String, DynasoarService>();
 
-	public static void start() {
-		// TODO: Start this in a separate thread
-		current = new ServiceMonitor();
-		th = new Thread(current, "ServiceMonitor");
-		th.start();
-	}
+    public static void start() {
+        // TODO: Start this in a separate thread
+        current = new ServiceMonitor();
+        th = new Thread(current, "ServiceMonitor");
+        th.start();
+    }
 
-	public static boolean isRunning() {
-		if (current == null) {
-			return false;
-		}
+    public static boolean isRunning() {
+        if (current == null) {
+            return false;
+        }
 
-		return th.isAlive();
-	}
+        return th.isAlive();
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
 
-		// Read "ServiceConfigDir" from configuration and starts listening
-		// to the directory
-		String serviceConfigDirPath = Configuration
-				.getConfig("serviceConfigDir");
-		DirectoryWatcher dir = new DirectoryWatcher(
-				new ServiceConfigChangeEvent());
-		dir.watch(serviceConfigDirPath);
+        // Reads "serviceConfigDir" from configuration and starts listening
+        // to the directory
+        String serviceConfigDirPath = Configuration.getConfig("serviceConfigDir");
+        DirectoryWatcher dir = new DirectoryWatcher(
+                new ServiceConfigChangeEvent());
+        dir.watch(serviceConfigDirPath);
 
-		// In case of any changes in directory, Read service config file,
-		// load/re-deploy the service on local server
+        // In case of any changes in directory, Read service config file,
+        // load/re-deploy the service on local server
 
-		// Notify NodeCommunicator of all the changes occurred
+        // Notify NodeCommunicator of all the changes occurred
 
-	}
+    }
 
-	/**
-	 * Implements ChangeEvent interface, which will handle directory change
-	 * events of Service Config Directory
-	 * 
-	 * @author Rakshit Menpara
-	 */
-	public static class ServiceConfigChangeEvent implements ChangeEvent {
+    /**
+     * Implements ChangeEvent interface, which will handle directory change
+     * events of Service Config Directory
+     *
+     * @author Rakshit Menpara
+     */
+    public static class ServiceConfigChangeEvent implements ChangeEvent {
 
-		@Override
-		public void fileCreated(String path) {
-			DynasoarService service = this.readServiceConfig(path);
-			serviceMap.put(service.getShortName(), service);
-		}
+        @Override
+        public void fileCreated(String path) {
+            DynasoarService service = null;
+            try {
+                service = this.readServiceConfig(path);
+                if (service.getdeployed().equals("true")) {
 
-		@Override
-		public void fileModified(String path) {
-			DynasoarService service = this.readServiceConfig(path);
-			serviceMap.put(service.getShortName(), service);
-		}
+                    // Copies the relative WAR file from WARdir directory to the deploy directory
 
-		@Override
-		public void fileRemoved(String path) {
-			// TODO: Correct
-			DynasoarService service = this.readServiceConfig(path);
-			serviceMap.remove(service.getShortName());
-		}
+                    String delims = "[.]";
+                    String[] tokens = path.split(delims);
+                    String warpath = tokens[0] + ".war";
+                    File warfile = new File("sample/WARdir/" + warpath);
+                    File deployfile = new File("sample/deploy/" + warpath);
 
-		private DynasoarService readServiceConfig(String path) {
-			DynasoarService service = new DynasoarService();
+                    this.copyFile(warfile, deployfile);
+                }
+            } catch (IOException ex) {
+                java.util.logging.Logger.getLogger(ServiceMonitor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            serviceMap.put(service.getShortName(), service);
+        }
 
-			// TODO: Read and parse the config file using JSON parser (jackson)
+        @Override
+        public void fileModified(String path) {
+            DynasoarService service = null;
+            try {
+                service = this.readServiceConfig(path);
+                if (service.getdeployed().equals("true")) {
 
-			return service;
-		}
-	}
+                    // Copies the relative WAR file from WARdir directory to the deploy directory
+
+                    String delims = "[.]";
+                    String[] tokens = path.split(delims);
+                    String warpath = tokens[0] + ".war";
+                    File warfile = new File("sample/WARdir/" + warpath);
+                    File deployfile = new File("sample/deploy/" + warpath);
+
+                    this.copyFile(warfile, deployfile);
+                }
+
+            } catch (IOException ex) {
+                java.util.logging.Logger.getLogger(ServiceMonitor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            serviceMap.put(service.getShortName(), service);
+        }
+
+        @Override
+        public void fileRemoved(String path) {
+            // TODO: Correct
+            DynasoarService service = null;
+            try {
+                service = this.readServiceConfig(path);
+            } catch (IOException ex) {
+                java.util.logging.Logger.getLogger(ServiceMonitor.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            serviceMap.remove(service.getShortName());
+        }
+
+        private DynasoarService readServiceConfig(String path) throws IOException {
+            DynasoarService service = new DynasoarService();
+
+            //Reads and parse the config file using JSON parser (jackson)
+
+            String relpath = "sample/serviceConfig/" + path;
+            Configuration.readConfiguration(relpath);
+            service.setShortName(Configuration.getConfig("name"));
+            service.setdeployed(Configuration.getConfig("deployed"));
+
+            return service;
+        }
+
+        public void copyFile(File sourceFile, File destFile) throws IOException {
+            if (!destFile.exists()) {
+                destFile.createNewFile();
+            }
+
+            FileChannel source = null;
+            FileChannel destination = null;
+
+            try {
+                source = new FileInputStream(sourceFile).getChannel();
+                destination = new FileOutputStream(destFile).getChannel();
+                destination.transferFrom(source, 0, source.size());
+            } finally {
+                if (source != null) {
+                    source.close();
+                }
+                if (destination != null) {
+                    destination.close();
+                }
+            }
+        }
+    }
 }
